70bcd5bd198cd086aa1da8afd6a45b4b67d38629
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2023-01-03 16:12:07 +0100

    more options

--- src/stracer_lib/main.ml
+++ src/stracer_lib/main.ml
@@ -27,7 +27,12 @@ type process = {
   mutable process : string option ;
 }
 
-let parse filename =
+let parse ?output filename =
+
+  let oc = match output with
+    | None -> stdout
+    | Some file -> open_out file
+  in
 
   let map = ref IntMap.empty in
   let first_process = ref None in
@@ -116,18 +121,25 @@ let parse filename =
           lines
     in
     List.iter (fun line ->
-        if EzString.starts_with line ~prefix:"clone(" ||
-           EzString.starts_with line ~prefix:"<... vfork resumed>" ||
-           EzString.starts_with line ~prefix:"<... clone resumed>"
+        if
+          EzString.starts_with line ~prefix:"clone(" ||
+          EzString.starts_with line ~prefix:"clone3(" ||
+          EzString.starts_with line ~prefix:"<... vfork resumed>" ||
+          EzString.starts_with line ~prefix:"<... clone resumed>" ||
+          EzString.starts_with line ~prefix:"<... clone3 resumed>" ||
+          false
         then
           let pid = get_retcode line in
           match pid with
           | None ->
-              Printf.eprintf "%d CAN NOT EXTRACT RETCODE %s\n%!" p.pid line
+              if Filename.check_suffix line "<unfinished ...>" then
+                ()
+              else
+                Printf.eprintf "%d CAN NOT EXTRACT RETCODE '%s'\n%!" p.pid line
           | Some pid ->
               match int_of_string pid with
               | exception _ ->
-                  Printf.eprintf "pid %d CAN NOT PARSE RETCODE %s\n%!" p.pid line
+                  Printf.eprintf "pid %d CAN NOT PARSE RETCODE '%s'\n%!" p.pid line
               | pid ->
                   (*                  Printf.printf "%sclone() = %d\n%!" indent pid; *)
                   let pp = try
@@ -147,10 +159,12 @@ let parse filename =
   in
 
   let rec print_process indent p =
-    Printf.printf "%s%d: %s\n%!" indent p.pid
+    Printf.fprintf oc "%s%d: %s (%d lines)\n%!" indent p.pid
       (match p.process with
        | Some process -> process
-       | None -> "<unknown>") ;
+       | None -> "<unknown>")
+      ( List.length p.lines )
+    ;
     List.iter (print_process (indent ^ "|---")) p.children ;
   in
   begin
@@ -158,28 +172,98 @@ let parse filename =
     | None -> assert false
     | Some p ->
         iter_process p ;
-
         print_process "" p ;
   end;
+
+
   IntMap.iter (fun _pid p ->
       if not p.printed then begin
         Printf.eprintf "ERROR: orphaned process !!!\n%!";
-        print_process "" p
+        print_process "" p;
+        List.iteri (fun i line ->
+            Printf.fprintf oc "   %d: %s\n%!" i line;
+          ) (List.rev p.lines) ;
       end
     ) !map ;
 
-
+  begin
+    match output with
+    | None -> ()
+    | Some _ -> close_out oc
+  end;
   ()
 
+
+
+
+
+
 let main () =
+  let something_done = ref false in
+  let exec_args = ref [] in
+  let strace_output = ref "strace.log" in
+  let output = ref None in
+  let exec_and_parse arg = exec_args := arg :: !exec_args in
 
   let arg_list = Arg.align [
 
+      "-o", Arg.String (fun s -> output := Some s),
+      "FILE where results should be stored";
+
+      "-f", Arg.String (fun s -> strace_output := s),
+      "FILE where strace log should be stored";
+
+      "--strace", Arg.Rest exec_and_parse,
+      "Run strace with all remaining arguments";
+
     ] in
-  let arg_anon filename = parse filename in
+  let arg_anon filename =
+    something_done := true ;
+    Printf.eprintf "Parsing %s\n%!" filename;
+    parse ?output:!output filename
+  in
+
   let arg_usage =
-    Printf.sprintf "%s [OPTIONS] [FILES]: parse strace output" Sys.argv.(0)
+    String.concat "\n" [
+      Printf.sprintf "%s [OPTIONS] [FILES]: parse strace output" Sys.argv.(0) ;
+      "" ;
+      "Example:";
+      "```";
+      "strace -o strace.log -f -e trace=process COMMAND";
+      "```";
+      "Use of '-o' and '-f' is mandatory. Use of '-s 1024' and '-v' can be useful.";
+    ]
   in
 
   Arg.parse arg_list arg_anon arg_usage ;
+
+  begin
+    match !exec_args with
+    | [] -> ()
+    | args ->
+        let args = List.rev args in
+        let cmd = [ "strace" ;
+                    "-o"; !strace_output ;
+                    "-f" ;
+                    "-e" ; "trace=process" ;
+                    "-s" ; "1024" ;
+                  ]
+                  @ args
+        in
+        Printf.eprintf "Calling %s\n%!"
+          (String.concat " " cmd );
+        let pid =
+          Unix.create_process
+            "strace"
+            ( Array.of_list cmd )
+            Unix.stdin Unix.stdout Unix.stderr
+        in
+        Printf.eprintf "Waiting for pid %d\n%!" pid;
+        let code, _status = Unix.waitpid [] pid in
+        Printf.eprintf "Command exited with code %d\n%!" code;
+        arg_anon !strace_output
+  end;
+
+  if not !something_done then
+    Printf.eprintf "%s\n%!" arg_usage;
   ()
